014e1d14689564b81cc6a0004d2a2b702d5c7b3a
selinaguo <selinaguo@tencent.com>
2023-03-30 20:36:45 +0800

    minor: 调整启动指令

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-${PYTHON_BIN} main.py --address=0.0.0.0 --port=30000 gameserver > /tmp/gameserver.log && ${PYTHON_BIN} user_server/main.py userserver > /tmp/userserver.log
+echo "begin starting gameserver"
+${PYTHON_BIN} main.py --address=0.0.0.0 --port=30000 gameserver > /tmp/gameserver.log &
+
+echo "begin starting userserver"
+${PYTHON_BIN} user_server/main.py userserver > /tmp/userserver.log &
 
 echo "start server finished" > /tmp/start.log
